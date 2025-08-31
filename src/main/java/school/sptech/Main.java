0c0b723fcb0ec3b00c3d3e68c1770f65c6723f19
AdamanLino <adaman.santos@sptech.school>
2025-08-31 13:52:12 -0300

    finalizado

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -1,8 +1,20 @@
 package school.sptech;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Time time = new Time();
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Dados do Time 1: ");
+        System.out.println("Nome: ");
+        String nome = input.nextLine();
+
+        System.out.println("Vitórias: ");
+        Integer vitorias = time.vitorias;
+
+        System.out.printf("Time: %s Vitórias: %d", nome, vitorias);
     }
 }
--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,73 @@
 package school.sptech;
 
 public class Time {
+    String nome;
+    Integer vitorias = 0;
+    Integer empates = 0;
+    Integer derrotas = 0;
 
+    public void registrarVitoria() {
+        vitorias++;
+    }
+
+    public void registrarEmpate() {
+        empates++;
+    }
+
+    public void registrarDerrota() {
+        derrotas++;
+    }
+
+    public Integer getPontos() {
+        Integer totalPontos;
+        Integer pontoVitorias = vitorias * 3;
+
+        return totalPontos = pontoVitorias + empates;
+    }
+
+    public Integer getTotalPartidas() {
+        Integer totalPartidas;
+
+        return totalPartidas = vitorias + empates + derrotas;
+    }
+
+    public Integer getAproveitamento() {
+        //Retorna o percentual de vitórias em relação ao total de partidas disputadas.
+        Integer perceptualV;
+        perceptualV = (vitorias * 100)/getTotalPartidas();
+
+        return perceptualV;
+    }
+
+    public void compararAproveitamento(Time adversario) {
+        if (getAproveitamento() > adversario.getAproveitamento()) {
+            System.out.println("O time %s tem um aproveitamento maior que o time %s".formatted(nome, adversario.nome));
+        } else if (getAproveitamento() < adversario.getAproveitamento()) {
+            System.out.println("O time %s tem um aproveitamento maior que o time %s".formatted(adversario.nome, nome));
+        } else {
+            System.out.println("Os times %s e %s tem o mesmo aproveitamento".formatted(nome, adversario.nome));
+        }
+    }
+
+    public void exibirInformacoes() {
+        var texto = """
+                Time: %s
+                Vitórias: %d
+                Empates: %d
+                Derrotas: %d
+
+                Pontos: %d
+                Total de Partidas: %d
+                Aproveitamento: %d%%
+                    """.formatted(
+                nome,
+                vitorias,
+                empates,
+                derrotas,
+                getPontos(),
+                getTotalPartidas(),
+                getAproveitamento());
+
+        System.out.println(texto);
+    }
 }
